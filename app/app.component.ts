11828ab42ffddf89c03967966bedfff3123024a6
pinku_4132 <pinku4132@gmail.com>
2017-04-20 16:03:21 +0530

    First commit:basicSPA

---
+++ app/app.component.ts
@@ -0,0 +1,28 @@
+import {Component} from 'angular2/core';
+import { NavbarComponent } from './nav-bar.component';
+import { RouteConfig ,ROUTER_DIRECTIVES} from 'angular2/router';
+import { HomeComponent} from './home.component';
+import { UsersComponent} from './users.component';
+import { PostsComponent} from './posts.component';
+@RouteConfig([
+    { path: '/home', name:'Home', component:HomeComponent,useAsDefault:true},
+    {path:'/users',name:'Users',component:UsersComponent},
+    {path:'/posts',name:'Posts',component:PostsComponent},
+    {path:'/*other',name:'Others',redirectTo:['Home']}
+])
+    
+@Component({
+    selector: 'my-app',
+    template: `
+        <navbar></navbar>
+        <div class="container">
+            <router-outlet></router-outlet>
+        </div>
+        
+    `,
+    directives:[NavbarComponent,ROUTER_DIRECTIVES]
+
+})
+export class AppComponent {
+
+ }
---
+++ app/home.component.ts
@@ -0,0 +1,9 @@
+import { Component} from 'angular2/core';
+import { RouteParams } from 'angular2/router';
+
+@Component({
+    template:'Home'
+})
+export class HomeComponent{
+
+}
