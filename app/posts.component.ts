910a1182697c7f1b53e2a3fb7b8bc0b3e16312e0
pinku_4132 <pinku4132@gmail.com>
2017-05-05 01:11:58 +0530

    Additional post comments

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from 'angular2/core';
+
 import {HTTP_PROVIDERS } from 'angular2/http';
 import { PostsService } from './posts.service';
 import { UsersService} from './users.service';
